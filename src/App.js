9490a79b1eea0bf08a91e8f468e4fc119fe5dc57
Randell Dawson <randy@randydawson.com>
2022-07-29 11:04:49 -0600

    feat: create todo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [task, setTask] = useState('')
+  const [todos, setTodos] = useState([])
+  const [globalID, setGlobalID] = useState(0)
+
+  function createTodo(event) {
+    // event.preventDefault()
+    setTodos(oldTodos => {
+      const id = globalID
+      setGlobalID(globalID + 1)
+      return [...oldTodos, { todo: task, id }]
+    })
+  }
+
+  function deleteItem(itemID) {
+    setTodos(oldTodos => {
+      return oldTodos.filter(({ id }) => id !== itemID)
+    })
+  }
+
+  return <div>
+    <h1>Best To Do App Ever</h1>
+      <input
+        type="text"
+        value={task}
+        onChange={event => setTask(event.target.value)}
+      />
+      <button onClick={createTodo}>Create Todo</button>
+    <ul>
+      {
+      todos.map(item => <div key={item.id}>
+          <li>
+            {item.todo}
+            <button onClick={deleteItem(item.id)}>Delete</button>
+          </li>
+        </div>
+      )
+    }
+    </ul>
+  </div>;
 }
 
 export default App;
